a3b238399f0a5e79a6abd6c36e3749a8916356fa
julian <julian@Dell630c>
2014-01-18 16:14:08 +0100

    First commit

---
+++ WebContent/Websocket.js
@@ -0,0 +1,26 @@
+var wsuri ="ws://"+document.location.host + document.location.pathname+"MESOEND";
+var websocket = new WebSocket(wsuri);
+var output = document.getElementById("output");
+websocket.onopen = function(evt) { onOpen(evt);};
+websocket.onerror = function(evt) { onError(evt);};
+websocket.onmessage = function(evt) { onMessage(evt);};
+
+function onError(evt)
+{
+	writeToScreen('<span style="Color: red;">ERROR:</span>'+evt.data);
+}
+//For testing purposes
+
+function writeToScreen(message){
+    output.innerHTML += message + "<br>";
+}
+
+function onOpen(evt){
+    writeToScreen("Connected to " + wsuri);
+    websocket.send("Hello Socket");
+}
+function onMessage(evt) {
+    console.log("received: " + evt.data);
+    writeToScreen(evt.data);
+}
+// End test functions
