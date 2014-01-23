d823c0237da3075ddb9bf4255343b8662f2ca97c
julian <julian@Dell630c>
2014-01-23 11:51:38 +0100

    q

---
+++ src/org/kniftosoft/entity/Maschine.java
@@ -0,0 +1,71 @@
+package org.kniftosoft.entity;
+
+import java.io.Serializable;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Maschine
+ *
+ */
+@Entity
+
+public class Maschine implements Serializable {
+
+	   
+	@Id
+	private int ID;
+	private String Name;
+	private String Standort;
+	private int Auftragsnr;
+	private int Auftragsgrösse;
+	private int Zustand;
+	private static final long serialVersionUID = 1L;
+
+	public Maschine() {
+		super();
+	}   
+	public int getID() {
+		return this.ID;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}   
+	public String getName() {
+		return this.Name;
+	}
+
+	public void setName(String Name) {
+		this.Name = Name;
+	}   
+	public String getStandort() {
+		return this.Standort;
+	}
+
+	public void setStandort(String Standort) {
+		this.Standort = Standort;
+	}   
+	public int getAuftragsnr() {
+		return this.Auftragsnr;
+	}
+
+	public void setAuftragsnr(int Auftragsnr) {
+		this.Auftragsnr = Auftragsnr;
+	}   
+	public int getAuftragsgrösse() {
+		return this.Auftragsgrösse;
+	}
+
+	public void setAuftragsgrösse(int Auftragsgröße) {
+		this.Auftragsgrösse = Auftragsgröße;
+	}   
+	public int getZustand() {
+		return this.Zustand;
+	}
+
+	public void setZustand(int Zustand) {
+		this.Zustand = Zustand;
+	}
+   
+}
---
+++ src/org/kniftosoft/entity/Test.java
@@ -0,0 +1,46 @@
+package org.kniftosoft.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the test database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Test.findAll", query="SELECT t FROM Test t")
+public class Test implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idTest = null;
+
+	private String value;
+
+	public Test() {
+	}
+
+	public int getIdTest() {
+		return this.idTest;
+	}
+
+	public void setIdTest(int idTest) {
+		this.idTest = idTest;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	@Override
+	public String toString(){
+		return "ID = " +idTest+" Value = "+value;
+	}
+
+}
