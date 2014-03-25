f5bb6401e13eef426607d961962449d961ca1fb4
julian <julian@192.168.1.100>
2014-03-25 07:55:13 +0100

    clientupdate
    Signed-off-by: julian <julian@192.168.1.100>

--- src/org/kniftosoft/entity/Log.java
+++ src/org/kniftosoft/entity/Log.java
@@ -1,7 +1,11 @@
 package org.kniftosoft.entity;
 
 import java.io.Serializable;
+
 import javax.persistence.*;
+
+import org.kniftosoft.update.Databaselistener;
+
 import java.sql.Timestamp;
 
 
@@ -10,6 +14,7 @@ import java.sql.Timestamp;
  * 
  */
 @Entity
+@EntityListeners(Databaselistener.class)
 @NamedQuery(name="Log.findAll", query="SELECT l FROM Log l")
 public class Log implements Serializable {
 	private static final long serialVersionUID = 1L;
